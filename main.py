2f0d9a5bfc40f2a77b3b42b18a04e7e0b0ae7fa1
Dark <youcefsegouat4@gmail.com>
2026-05-05 12:33:06 +0100

    size

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from gui_components import GameWindow
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = GameWindow()
-    window.resize(1000, 600)
+    window.resize(1290, 600)
     window.show()
     sys.exit(app.exec())
